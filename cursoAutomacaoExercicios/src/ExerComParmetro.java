e745d84dd88282fa6adf72c02d249b039fd35dc8
Ricardo Nere <ricardonere@gmail.com>
2023-05-06 10:15:09 -0300

    Commit2

---
+++ cursoAutomacaoExercicios/src/ExerComParmetro.java
@@ -0,0 +1,11 @@
+public class ExerComParmetro {
+    public static String texto (String primeiroTexto,String segundoTexto){
+        
+        return primeiroTexto+segundoTexto;
+}
+public static void main (String[]args){
+    String primeiroTexto = "Primeiro texto leitura";
+    String segundoTexto = "Segundo texto concatena";
+System.out.println(texto(primeiroTexto, segundoTexto));
+}
+}
